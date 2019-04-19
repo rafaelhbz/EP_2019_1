ccc5d1ea32eb611f92c48f0c8ff44bb20ca4ffd3
joaopfa2 <49791263+joaopfa2@users.noreply.github.com>
2019-04-19 19:19:02 -0300

    Update ep1.py

--- ep1.py
+++ ep1.py
@@ -72,8 +72,9 @@ def main():
             game_over = True
         else:
 
-            # Aluno B: substitua este comentário e a linha abaixo pelo código
-            # para pedir a escolha do usuário.
+
+            print (opcoes)
+            escolha = input("faça sua ecolha, jovem cafanhoto: ")
             escolha = ""
 
             if escolha in opcoes:
