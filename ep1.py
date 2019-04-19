8ccef8874116627800249e552df8c40cbdc7e033
RafaelHBZ <rafaelrhbz@gmail.com>
2019-04-19 19:33:04 -0300

    Aqui o Rafael Zanfolin adicionou novos cenários, modificou os antigos e tentou fazer um inventário

--- ep1.py
+++ ep1.py
@@ -3,45 +3,58 @@
 # Alunos: 
 # - aluno A: Rafael Henrique Belini Zanfolin, rafaelhbz@insper.edu.br
 # - aluno B: João Pedro Farias Araujo, joaopfa2@al.insper.edu.br
-
 def carregar_cenarios():
     cenarios = {
         "inicio": {
             "titulo": "Saguao do perigo",
             "descricao": "Voce esta no saguao de entrada do insper",
             "opcoes": {
-                "andar professor": "Tomar o elevador para o andar do professor",
+                "primeiro andar": "Tomar o elevador para segundo andar",
                 "biblioteca": "Ir para a biblioteca"
             }
         },
-        "andar professor": {
-            "titulo": "Andar do desespero",
-            "descricao": "Voce chegou ao andar da sala do seu professor",
+        "primeiro andar": {
+            "titulo": "O misterioso andar",
+            "descricao": "Voce chegou ao primeiro andar",
             "opcoes": {
                 "inicio": "Tomar o elevador para o saguao de entrada",
-                "professor": "Falar com o professor"
+                "sala 101": "Ir para a sala 101",
+                "sala 102": "Ir para a sala 102",
+                "guardião do portão": "Falar com o guardião do portão"
             }
         },
-        "professor": {
-            "titulo": "O monstro do Python",
-            "descricao": "Voce foi pedir para o professor adiar o EP. "
-                         "O professor revelou que é um monstro disfarçado "
-                         "e devorou sua alma.",
-            "opcoes": {}
+        "guardião do portão": {
+            "titulo": "A passagem para o segundo andar",
+            "descricao": "Voce foi pedir para subir ao segundo andar"
+                         "O guardião do portão pede 10 gold coins para subir ao segundo andar ",
+            "opcoes": {"primeiro andar" : "Voltar para o primeiro andar e buscar mais recursos",
+                       "segudno andar": "Pagar o guardião do portão e subir para o segundo andar"}
         },
         "biblioteca": {
             "titulo": "Caverna da tranquilidade",
             "descricao": "Voce esta na biblioteca",
             "opcoes": {
-                "inicio": "Voltar para o saguao de entrada"
-            }
+                "inicio": "Voltar para o saguao de entrada",
+                "aquario 11": "Entrar para o aquario 11"}
+        },
+        "aquario 11": {
+                "titulo":"Santuario das indecisoes",
+                "descricao": "Voce esta no Santuario das decisoes",
+                "opcoes": {"alavanca": "Puchar a alavanca",
+                           "biblioteca": "Voltar para a biblioteca"}
+        },
+        "alavanca" : {
+                "titulo":"Parabens, você teve coragem de encarar o seu destino",
+                "descricao":"Você obteve 10 gold coins",
+                "opcoes": {"biblioteca":"Voltar para a biblioteca"}}
         }
-    }
     nome_cenario_atual = "inicio"
     return cenarios, nome_cenario_atual
 
 
 def main():
+    gold_pouch = 0
+    inventario = []
     print("Na hora do sufoco!")
     print("------------------")
     print()
@@ -59,9 +72,6 @@ def main():
     while not game_over:
         cenario_atual = cenarios[nome_cenario_atual]
 
-        # Aluno A: substitua este comentário pelo código para imprimir 
-        # o cenário atual.
-
         print(cenario_atual['titulo'])
         print('-'*len(cenario_atual['titulo']))
         print(cenario_atual['descricao'])
@@ -74,10 +84,17 @@ def main():
 
             # Aluno B: substitua este comentário e a linha abaixo pelo código
             # para pedir a escolha do usuário.
-            escolha = ""
+            print(opcoes)
+            escolha = input("Faça a sua escolha, jovem gafanhoto: ")
 
             if escolha in opcoes:
                 nome_cenario_atual = escolha
+                if escolha == cenarios["alavanca"]:
+                    gold_pouch = gold_pouch + 10
+                    print(gold_pouch)
+                else:
+                    continue
+                print("10 gold coins foram adicionados à sua gold pouch")
             else:
                 print("Sua indecisão foi sua ruína!")
                 game_over = True
